1dd9e9056bdf1cf32707b928c7776f802a4ee301
Ricardo Marañón <ledijolasartenalcazo@gmail.com>
2017-06-14 09:07:51 +0200

    Initial Commit

---
+++ SoldierGame/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SoldierGame
+//
+//  Created by cice on 14/6/17.
+//  Copyright © 2017 Cice. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
